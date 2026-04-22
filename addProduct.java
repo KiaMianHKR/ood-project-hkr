9e42a07d3065b6373a0ae44429e5601c73bb8730
Kiasar Mian <kiasar.mian@hkr.se>
2026-04-22 11:20:36 +0200

    a

--- addProduct.java
+++ addProduct.java
@@ -6,4 +6,8 @@ public class addProduct {
     // SOMETHING NEW
 
     // WRITING SomE MORE CODE
+
+    public void createProduct(String name, double price, int quantity) {
+        System.out.println(name);
+    }
 }
